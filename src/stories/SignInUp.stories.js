9b8cca3dbcfd3353d8c5e5aaab9a22e5f1fdfaed
Dhruv <dhruv27kapri@gmail.com>
2024-02-09 20:11:51 +0530

    Sign up page started

---
+++ src/stories/SignInUp.stories.js
@@ -0,0 +1,15 @@
+import SignInUp from "../Components/SignInUp";
+
+export default {
+  title: "Example/SignInUp",
+  component: SignInUp,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: "fullscreen",
+  },
+};
+
+export const SignIn = {};
+export const SignUp = {};
